9d08ff0a3cf5c13b987d27cab49df049d0520776
micky <363755755@qq.com>
2018-08-11 00:12:49 +0800

    有关于提交GitHab的基础代码提交

---
+++ src/main/java/com/mooc/house/HouseApplication.java
@@ -0,0 +1,16 @@
+package com.mooc.house;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@SpringBootApplication
+@MapperScan("com.mooc.house.mapper")
+@EnableAsync
+public class HouseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HouseApplication.class, args);
+    }
+}
